43015226b1f5cee9cbcc6c3c30739cf52d1fb67a
karandeepkalra <karandeepkaur0089@gmail.com>
2025-04-04 20:34:43 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   base: "./", 
   server: {
+    allowedHosts:true,
     host: '0.0.0.0', // Listen on all addresses
     port: 3000,      // Optional: specify a port
   }
